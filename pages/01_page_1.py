8b0f8015426dba5b7b54d7a0b27b527eecdd6527
Clément LION <clement.lion@saegus.com>
2024-06-09 13:22:35 +0200

    Added exercise text

--- pages/01_page_1.py
+++ pages/01_page_1.py
@@ -1,3 +1,6 @@
 import streamlit as st
 
 st.title("Page 1")
+
+st.markdown("""Modify this page so that you have a page displaying important KPI on one band (the user can choose the band). \n
+            Interesting KPIs include: number of songs per year. Wordcloud, most popular songs, short description, first song date, last song date...""")
--- pages/02_page_2.py
+++ pages/02_page_2.py
@@ -1,3 +1,6 @@
 import streamlit as st
 
 st.title("Page 2")
+
+st.markdown("""Modify this page so that you have a page displaying important KPI for all our bands \n
+            For example, cluster the songs using kmeans...""")
